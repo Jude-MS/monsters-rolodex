f4c54ad4c9535bfeef80d628de0e13025d6d03a9
Jude Monel Sanon <jsanon@tibco-support.com>
2020-04-11 18:20:57 -0500

    Updates

---
+++ precache-manifest.e866c6df1ed5656776d9d44cf807d070.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c37fcaffd0eb1cd339ab96b7e32052c",
+    "url": "/Jude-MS.github.io/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "473fd1be463d6cf36980",
+    "url": "/Jude-MS.github.io/monsters-rolodex/static/css/main.fa7eed69.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/Jude-MS.github.io/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Jude-MS.github.io/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "473fd1be463d6cf36980",
+    "url": "/Jude-MS.github.io/monsters-rolodex/static/js/main.cedc5e44.chunk.js"
+  },
+  {
+    "revision": "f3c6599082e9fb216fc5",
+    "url": "/Jude-MS.github.io/monsters-rolodex/static/js/runtime-main.c7fca23f.js"
+  }
+]);
